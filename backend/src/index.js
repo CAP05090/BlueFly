35cf4d4cbb161eb5099bdbcd1602c10435cda5c4
CAP05090 <ksand7398@gmail.com>
2024-03-11 08:32:57 +0530

    setup mongodb connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const dotenv = require("dotenv").config()
 const cors = require("cors")
 
+const { connection } = require("./configs/db")
+
 const app = express()
 const PORT = process.env.PORT
 
@@ -14,6 +16,7 @@ app.get("/", (req, res)=>{
 
 app.listen(PORT, async()=>{
     try {
+        await connection
         console.log(`Express is running on port ${PORT} and DB is connected`)
     } catch (error) {
         console.log(error.message)
